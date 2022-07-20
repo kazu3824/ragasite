d192c12b483b2c85e5c71ba3c644c76c627475e2
kazu3824 <zebest3824@gmail.com>
2022-07-20 10:54:28 +0000

    [fix]topページボタン表示の制御

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-
+  
   GUEST_EMAIL = "guest@example.com"
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
