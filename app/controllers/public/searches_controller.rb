b201c673e3aa8ca3f85faf4d580278a91e38f79c
kazu3824 <zebest3824@gmail.com>
2022-07-27 10:20:02 +0000

    [fix]santize_sql_like追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -11,10 +11,10 @@ class Public::SearchesController < ApplicationController
     #トラックテーブルとアーティストテーブルを同時に指定して曲名と概要と歌手名の中に,パラメーターとして送られてきたキーワードに一致するデータがあれば取得する
     @tracks = Track.joins(:artist, :tag)
               .where("tracks.title like ? or tracks.description like ? or artists.name like ? or tags.name like ?",
-              "%" + params["track"]["keyword"] + "%",
-              "%" + params["track"]["keyword"] + "%",
-              "%" + params["track"]["keyword"] + "%",
-              "%" + params["track"]["keyword"] + "%")
+              "%" + santize_sql_like(params["track"]["keyword"]) + "%",
+              "%" + santize_sql_like(params["track"]["keyword"]) + "%",
+              "%" + santize_sql_like(params["track"]["keyword"]) + "%",
+              "%" + santize_sql_like(params["track"]["keyword"]) + "%")
     #パラメーターとして送られてきたキーワードをseach_wordに代入している
     @seach_word = params[:track][:keyword]
     render 'public/searches/index'
