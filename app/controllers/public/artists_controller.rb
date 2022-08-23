cce03fe351ad90eace6242e972ad345cfe155af1
kazu3824 <zebest3824@gmail.com>
2022-08-23 15:25:49 +0000

    artist詳細ページネーション追加

--- app/controllers/public/artists_controller.rb
+++ app/controllers/public/artists_controller.rb
@@ -1,6 +1,13 @@
 class Public::ArtistsController < ApplicationController
   def show
-     @artist = Artist.find(params[:id])
-     @tracks = @artist.tracks
+    @artist = Artist.find(params[:id])
+    @tracks = @artist.tracks
+    # Kaminariの配列版を使用して@tracksをページネーションする
+    @tracks = Kaminari.paginate_array(@tracks).page(params[:page]).per(10)
+    # リクエストに応じてビューの切り替え
+    respond_to do |format|
+      format.html # 非同期通信でない場合はhtml.erbを呼ぶ
+      format.js # 非同期の場合はjs.erbを呼ぶ
+    end
   end
 end
